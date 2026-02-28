ffa819e8470a09ac4a6eaeae09743388ab133a81
daniqss <danielqueijo14@gmail.com>
2026-02-28 03:29:39 +0100

    setup python env

--- flake.nix
+++ flake.nix
@@ -2,27 +2,22 @@
   description = "stepcode";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
+  outputs = {nixpkgs, ...}: let
     eachSystem = f:
       nixpkgs.lib.genAttrs ["x86_64-linux" "aarch64-linux"]
-      (system: f system (import nixpkgs {inherit system;}));
+      (system: f (import nixpkgs {inherit system;}));
   in {
-    devShells = eachSystem (system: pkgs: {
+    devShells = eachSystem (pkgs: {
       default = pkgs.mkShell {
         buildInputs = with pkgs; [
-          nodejs
-          pnpm
+          python314
+          uv
+          ruff
+          ty
         ];
-
-        shellHook = ''
-          export PATH="$PWD/node_modules/.bin:$PATH"
-        '';
       };
     });
   };
